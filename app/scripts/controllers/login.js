134acec0872fdaa85078a0b38752295797bc048a
wing <wing_kung@live.com>
2014-06-27 17:49:07 +0800

    vccbar

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,7 +2,8 @@
  * Created by wing on 2014/6/4.
  */
 var app = angular.module('vccbarApp');
-app.controller('LoginCtrl', function($scope, socket, $timeout){
+app.controller('LoginCtrl', function($scope, socket){
+    $scope.agentId = '101';
     $scope.login = function(){
         socket.emit('login', {tenantId: tenantId, agentId: $scope.agentId, password: $scope.password, ext: $scope.ext})
     };
@@ -12,5 +13,6 @@ app.controller('LoginCtrl', function($scope, socket, $timeout){
 
         }else{
         }
-    })
+    });
+
 });
